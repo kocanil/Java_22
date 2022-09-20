b626e5e2fe7340f8c30a01f7ed704f9ba9eff00d
anilk <anilk@LAPTOP-1OHNM9JD>
2022-09-20 17:35:01 +0300

    2

--- If/src/Main.java
+++ If/src/Main.java
@@ -2,7 +2,20 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int num = 20;
+		
+		if (num>20) {
+			System.out.println("sayı 20 den büyüktür");	
+		}
+		
+		if (num<20) {
+			System.out.println("sayı 20 den küçüktür");
+		}
+		
+		else {
+			System.out.println("sayı 20 ye eşittir");
+		}
+		
 
 	}
 
