423be2ebdfc5bf781afe2c7ea58882430ab655e0
anilk <anilk@LAPTOP-1OHNM9JD>
2022-09-20 16:48:53 +0300

    2

--- Variables/src/Main.java
+++ Variables/src/Main.java
@@ -2,8 +2,15 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("helloWorld");
+		int MatchPlayed = 9;
+		int MatchWon = 6;
+		double MatchPlayedNew = MatchPlayed;
+		double MatchWonNew = MatchWon;
+		double Result =  (MatchWonNew*100)/MatchPlayedNew;
+		
+		System.out.println("Win percantage is: % " + Result);
+		
+		
 	}
 
 }
