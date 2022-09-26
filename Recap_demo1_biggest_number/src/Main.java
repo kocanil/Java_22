d5edc7636d9feb38b95d00269ebd174035e265cf
anilk <anilk@LAPTOP-1OHNM9JD>
2022-09-26 11:01:27 +0300

    09_26_2022_v1

---
+++ Recap_demo1_biggest_number/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int num1 = 20;
+		int num2 = 14;
+		int num3 = 25;
+		int bigNum = num1;
+		
+		if (bigNum<num2) {
+			bigNum = num2;
+		}
+		
+		if (bigNum<num3) {
+			bigNum = num3;
+		}
+		
+		System.out.println(bigNum);
+		
+		
+
+	}
+
+}
