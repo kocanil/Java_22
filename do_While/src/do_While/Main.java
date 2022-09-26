2065a6876295497e69632517fc926493a3519a51
anilk <anilk@LAPTOP-1OHNM9JD>
2022-09-26 16:29:58 +0300

    09_26_2022_v1

---
+++ do_While/src/do_While/Main.java
@@ -0,0 +1,15 @@
+package do_While;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		int j =1;
+		
+		do {
+			System.out.println(j);
+			j+=3;
+		} while (j<40);
+		System.out.println("end of do-while loop");
+		System.out.println("____________________");
+	}
+}
