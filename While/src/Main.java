f0adacf8431369f3bf69183d3b09f636773519f4
anilk <anilk@LAPTOP-1OHNM9JD>
2022-09-26 14:19:16 +0300

    09_26_2022_v1

---
+++ While/src/Main.java
@@ -0,0 +1,28 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int i = 1;
+		while (i < 11) {
+			System.out.println(i);
+			i++;
+		}
+		System.out.println("End of full While loop");
+		System.out.println("_________________________");
+		int j = 1;
+		while (j < 10) {
+			System.out.println(j);
+			j += 2;
+		}
+		System.out.println("End of odd While loop");
+		System.out.println("_________________________");
+		int k = 2;
+		while (k < 11) {
+			System.out.println(k);
+			k += 2;
+		}
+		System.out.println("End of even While loop");
+		System.out.println("_________________________");
+	}
+
+}
