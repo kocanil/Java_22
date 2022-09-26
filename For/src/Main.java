9b30095a0edf0e6d7de9878746268bc035ef4237
anilk <anilk@LAPTOP-1OHNM9JD>
2022-09-26 13:43:18 +0300

    09_26_2022_v1

---
+++ For/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 11; i++) {
+			System.out.println(i);
+		}
+		System.out.println("________________");
+
+		for (int j = 0; j < 11; j += 2) {
+			System.out.println(j);
+		}
+		System.out.println("________________");
+
+		for (int j = 1; j < 10; j += 2) {
+			System.out.println(j);
+		}
+
+	}
+
+}
